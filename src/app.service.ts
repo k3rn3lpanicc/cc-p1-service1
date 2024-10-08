94b0958d28c46e81348c63374797c21c28874a61
Mohammad matin <matin.ghiasvand1381@gmail.com>
2024-10-08 15:54:56 +0330

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,7 +8,7 @@ export class AppService {
 
   handleFileUpload(file: Express.MulterS3File, email: string) {
     // Handle the uploaded file information and email
-    console.log(JSON.stringify(file));
+    console.log(`file: ${file}`);
     console.log(`File uploaded to S3: ${file.location}`);
     console.log(`Email: ${email}`);
     // Return file details and email confirmation
