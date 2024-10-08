a031ba14e14e1458bd083ebdebd4ae7ac402399d
Mohammad matin <matin.ghiasvand1381@gmail.com>
2024-10-08 15:29:24 +0330

    Add logs  to s3 upload

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,9 +8,9 @@ export class AppService {
 
   handleFileUpload(file: Express.MulterS3File, email: string) {
     // Handle the uploaded file information and email
+    console.log(JSON.stringify(file));
     console.log(`File uploaded to S3: ${file.location}`);
     console.log(`Email: ${email}`);
-
     // Return file details and email confirmation
     return {
       message: 'File uploaded successfully to S3!',
