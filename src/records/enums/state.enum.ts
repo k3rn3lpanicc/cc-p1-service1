07c2a51d96e3a9e801b26b58c2dee767a96a3cfa
Mohammad matin <matin.ghiasvand1381@gmail.com>
2024-10-11 18:47:29 +0330

    Add a new step to states

--- src/records/enums/state.enum.ts
+++ src/records/enums/state.enum.ts
@@ -1,8 +1,9 @@
 /* eslint-disable prettier/prettier */
 export const STATE = {
-  CREATED: 'CREATED',
-  PENDING: 'PENDING',
-  FAILED: 'FAILED',
-  DONE: 'DONE',
-  SENT: 'SENT',
+  CREATED: 'CREATED', // image uploaded, message in rmq sent
+  PENDING: 'PENDING', // message in rmq received, processing
+  READY: 'READY', // caption generated
+  FAILED: 'FAILED', // something went wrong
+  DONE: 'DONE', // new image generated
+  SENT: 'SENT', // email sent
 };
