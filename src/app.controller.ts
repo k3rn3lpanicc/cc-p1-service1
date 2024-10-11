4b0846244cdf01412791fcf866771ab6b94dd466
Mohammad matin <matin.ghiasvand1381@gmail.com>
2024-10-11 17:48:24 +0330

    bug fix: handle file upload to s3

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,57 +1,59 @@
 import {
   Body,
   Controller,
-  Get,
   Post,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
-import { AppService } from './app.service';
 import { FileInterceptor } from '@nestjs/platform-express';
-import * as AWS from 'aws-sdk';
-import * as multerS3 from 'multer-s3';
-
-const s3 = new AWS.S3({
-  endpoint: process.env.LIARA_ENDPOINT,
-  accessKeyId: process.env.LIARA_ACCESS_KEY,
-  secretAccessKey: process.env.LIARA_SECRET_KEY,
-  region: 'default',
-});
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { ConfigService } from '@nestjs/config';
+import { promises as fs } from 'fs';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private readonly s3: S3Client;
 
-  @Get()
-  checkHealth(): string {
-    return this.appService.checkHealth();
+  constructor(private readonly configService: ConfigService) {
+    this.s3 = new S3Client({
+      endpoint: this.configService.get<string>('LIARA_ENDPOINT'),
+      credentials: {
+        accessKeyId: this.configService.get<string>('LIARA_ACCESS_KEY'),
+        secretAccessKey: this.configService.get<string>('LIARA_SECRET_KEY'),
+      },
+      region: 'default',
+    });
   }
 
-  @Post('/service')
-  @UseInterceptors(
-    FileInterceptor('image', {
-      storage: multerS3({
-        s3: s3,
-        bucket: process.env.LIARA_BUCKET_NAME, // Your S3 bucket name
-        acl: 'public-read', // Set access control (can be public-read or private)
-        key: (req, file, cb) => {
-          // Set a custom file name
-          const fileName = `${Date.now().toString()}-${file.originalname}`;
-          cb(null, fileName);
+  @Post('service')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file, @Body('email') email) {
+    console.log({ file });
+    console.log({ email });
+    // const fileContent = await fs.readFile(file.buffer);
+
+    const uploadParams = {
+      Bucket: this.configService.get<string>('LIARA_BUCKET_NAME'),
+      Key: file.originalname,
+      Body: file.buffer,
+      ContentType: file.mimetype,
+    };
+
+    try {
+      await this.s3.send(new PutObjectCommand(uploadParams));
+      await fs.unlink(file.path); // Remove file after upload
+
+      return {
+        status: 'success',
+        message: 'File uploaded!',
+        url: {
+          name: file.originalname,
+          type: file.mimetype,
         },
-      }),
-      fileFilter: (req, file, callback) => {
-        if (!file.mimetype.match(/\/(jpg|jpeg|png)$/)) {
-          return callback(new Error('Only image files are allowed!'), false);
-        }
-        callback(null, true);
-      },
-    }),
-  )
-  service(
-    @UploadedFile() file: Express.MulterS3File,
-    @Body('email') email: string,
-  ) {
-    return this.appService.handleFileUpload(file, email);
+      };
+    } catch (error) {
+      console.error('Error uploading file:', error);
+      throw error;
+    }
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,17 +5,4 @@ export class AppService {
   checkHealth(): string {
     return 'Service is running';
   }
-
-  handleFileUpload(file: Express.MulterS3File, email: string) {
-    // Handle the uploaded file information and email
-    console.log(`file: ${file}`);
-    console.log(`File uploaded to S3: ${file.location}`);
-    console.log(`Email: ${email}`);
-    // Return file details and email confirmation
-    return {
-      message: 'File uploaded successfully to S3!',
-      fileUrl: file.location, // This contains the URL to the file in S3
-      email: email,
-    };
-  }
 }
